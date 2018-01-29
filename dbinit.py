5f4cacd4921e6ce97f77a4e0966df6c7d4473b5d
christopherjewell <christophernjewell@gmail.com>
2018-01-29 02:03:44 -0600

    Think it is finally working

--- dbinit.py
+++ dbinit.py
@@ -3,17 +3,21 @@ import datetime
 
 db = sqlite3.connect("test.db")
 # db = sqlite3.connect(":memory:")
-db.execute("create table records(timestamp, levels)")
+db.execute("create table records(timestamp, year, month, day, hour, levels)")
 
 with open('testdata.csv', 'r', buffering=1) as infile:
     for line_buffer in infile:
         record = line_buffer.split(sep=',')
+        time_format = "%Y-%m-%dT%H"
+        d_obj = datetime.datetime.strptime(record[0], time_format)
         record[1] = record[1].strip()
-        db.execute("insert into records(timestamp, levels) values (?, ?)", record)
+        new_record = [record[0], d_obj.year, d_obj.month, d_obj.day, d_obj.hour, record[1]]
+        db.execute("insert into records(timestamp, year, month, day, hour, levels) values (?, ?, ?, ?, ?, ?)", new_record)
+        db.commit()
 
-for row in db.execute("select timestamp, levels from records"):
-    print(row)
+# for row in db.execute("select timestamp, levels from records"):
+#     # print(type(row[0]))
+#     print(row[0])
 
-db.commit()
 db.close()
 
--- dbquery.py
+++ dbquery.py
@@ -2,16 +2,41 @@ import sqlite3
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+yearsFmt = mdates.DateFormatter('%Y')
+time_format = "%Y-%m-%dT%H"
 
 db = sqlite3.connect("test.db")
 
-# for row in db.execute("select timestamp, levels from records"):
-    # np.datetime64(row[0])
 
-mydtype = np.dtype([('f0', "datetime64[h]"),('f1','<i8')])
+timestamps = []
+levels = []
+# my_query = db.execute("select timestamp, levels from records")
+my_query = db.execute("select timestamp, levels from records where year=2018 and month=1 and day between 1 and 5")
 
-data = db.execute("select timestamp, levels from records")
-npdata = np.asarray(data, dtype=mydtype)
-plt.plot(data['f0'], data['f1'])
+for record in my_query:
+    timestamps.append(datetime.datetime.strptime(record[0], time_format))
+    levels.append(record[1])
+
+timestamps = np.asarray(timestamps)
+levels = np.asarray(levels)
+
+fig, ax = plt.subplots()
+ax.plot(timestamps, levels)
+
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(yearsFmt)
+ax.xaxis.set_minor_locator(months)
+
+# datemin = datetime.date(timestamps.min().year, 1, 1)
+# datemax = datetime.date(timestamps.max().year + 1, 1, 1)
+# ax.set_xlim(datemin, datemax)
+
+fig.autofmt_xdate()
+
 plt.show()
 
+# plt.plot(timestamps, levels)
